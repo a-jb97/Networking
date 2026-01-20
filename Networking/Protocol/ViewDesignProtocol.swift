fa280ed578f95f4083541f7263321ccfb8799362
a-jb97 <a_jb97@naver.com>
2026-01-20 17:19:51 +0900

    Feat : ViewDesignProtocol 설정

---
+++ Networking/Protocol/ViewDesignProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  Networking
+//
+//  Created by 전민돌 on 1/20/26.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol: AnyObject {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
