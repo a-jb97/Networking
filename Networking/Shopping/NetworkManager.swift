4f9de4f968c3f64990ee8f8d2bc8631c60bd8eb6
a-jb97 <a_jb97@naver.com>
2026-01-21 22:02:24 +0900

    Feat : NetworkManager struct를 싱글톤 패턴으로 생성해서 네트워크 통신 메서드 구현

---
+++ Networking/Shopping/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  Networking
+//
+//  Created by 전민돌 on 1/21/26.
+//
+
+import Foundation
+import Alamofire
+
+struct NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {  }
+    
+    func callRequest<T: Decodable>(query: String, start: Int, sort: String, type: T.Type, success: @escaping (T) -> Void) {
+        let url = "https://openapi.naver.com/v1/search/shop.json"
+        let headers: HTTPHeaders = ["X-Naver-Client-Id":APIKey.clientID, "X-Naver-Client-Secret":APIKey.secretID]
+        let parameters: Parameters = ["query":query, "display":"100", "start":start, "sort":sort]
+        
+        AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.queryString, headers: headers)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: T.self) { response in
+                switch response.result {
+                case .success(let value):
+                    dump(value)
+                    success(value)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+}
---
+++ Networking/Shopping/Shopping.swift
@@ -0,0 +1,8 @@
+//
+//  Shopping.swift
+//  Networking
+//
+//  Created by 전민돌 on 1/21/26.
+//
+
+import Foundation
