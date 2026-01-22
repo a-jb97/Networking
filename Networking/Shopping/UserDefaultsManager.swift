a9a263090e970511bf05ab1b7f36f9463bbcaea9
a-jb97 <a_jb97@naver.com>
2026-01-22 17:18:16 +0900

    Feat : 검색 키워드 저장을 위한 UserDefaultsManager 구현

--- Networking/Shopping/UserDefaultsManager.swift
+++ Networking/Shopping/UserDefaultsManager.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let value: T
+    
+    var wrappedValue: T {
+        get { UserDefaults.standard.object(forKey: self.key) as? T ?? self.value }
+        set { UserDefaults.standard.set(newValue, forKey: self.key) }
+    }
+}
+
+class UserDefaultsManager {
+    @UserDefault(key: "searchKeyword", value: nil)
+    static var nickname: [String]?
+}
