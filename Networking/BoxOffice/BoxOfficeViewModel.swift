b0045507f5004de19a8c843826e69b854c96a1c0
a-jb97 <a_jb97@naver.com>
2026-02-25 22:39:47 +0900

    Feat : BoxOfficeViewModel 구현

--- Networking/BoxOffice/BoxOfficeViewModel.swift
+++ Networking/BoxOffice/BoxOfficeViewModel.swift
@@ -13,15 +13,47 @@ final class BoxOfficeViewModel {
     let disposeBag = DisposeBag()
     
     struct Input {
-        
+        let dateKeyword: ControlProperty<String>
+        let searchButtonTap: ControlEvent<Void>
     }
     
     struct Output {
-        
+        let boxOfficeItems: Driver<[DailyBoxOffice]>
+        let errorMessage: PublishSubject<String>
     }
     
     func transform(input: Input) -> Output {
+        let boxOfficeItems: BehaviorRelay<[DailyBoxOffice]> = BehaviorRelay(value: [])
+        let errorMessage: PublishSubject<String> = PublishSubject()
         
-        return Output()
+        input.searchButtonTap
+            .withLatestFrom(input.dateKeyword)
+            .flatMap { keyword in
+                if self.isValidDateFormat(text: keyword) {
+                    return CustomObservable.singleBoxOfficeRequest(date: keyword)
+                } else {
+                    errorMessage.onNext("날짜 형식에 맞게 입력해주세요 (yyyyMMdd)")
+                    return Single<Result<[DailyBoxOffice], BoxOfficeNetworkError>>.never()
+                }
+            }
+            .bind(with: self) { owner, result in
+                switch result {
+                case .success(let value):
+                    boxOfficeItems.accept(value)
+                case .failure(let error):
+                    errorMessage.onNext(error.description)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(boxOfficeItems: boxOfficeItems.asDriver(), errorMessage: errorMessage)
+    }
+    
+    private func isValidDateFormat(text: String) -> Bool {
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyyMMdd"
+        
+        return formatter.date(from: text) != nil
     }
 }
